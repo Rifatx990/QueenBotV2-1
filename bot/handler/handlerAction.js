bc4f7677e08821ebb62b1dc5fbc9c5ff3d416466
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-05-19 12:29:00 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "🚸","😡","🙂"){
+        if (["🚸", "😡", "🙂"].includes(event.reaction)) {
   if(event.userID == "100084175784749"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
